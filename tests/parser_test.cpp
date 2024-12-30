b7988c6addfc903b7d4c7ea70217f69a15721a83
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-12-30 11:08:40 +0000

    Removed test cases and added skeleton comments for parsing

---
+++ tests/parser_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "dragon/lexer.h"
+#include "dragon/token.h"
+#include "dragon/parser.h"
+
+// Variable/Constant Declaration Node
+// Variable/Constant Assignment Node
+// While Loop Node
+// For Loop Node
+// If-Else Node
+// Function Call Node
