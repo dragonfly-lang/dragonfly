91a492fe6c7ba7f15b9f9775c9458d1734f56b2f
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2025-01-05 16:30:08 +0000

    Added all methods and data for lexer class

--- include/dragon/lexer.h
+++ include/dragon/lexer.h
@@ -7,3 +7,32 @@
 #include "token.h"
 
 class Lexer {
+public:
+    Lexer() = default;
+    Lexer(std::string input);
+
+    std::vector<Token> lex(std::string input);  
+    std::vector<Token> lex();
+
+    void reset();
+
+    Token lex_identifier();
+    Token lex_number();
+    Token lex_string();
+    Token lex_symbol();
+    Token lex_single_line_comment();
+    Token lex_multi_line_comment();
+
+    TokenType get_keyword(std::string input);
+
+private:
+    std::vector<Token> tokens;
+    std::string input;
+    size_t index = 0;
+    size_t line = 1;
+    size_t column = 1;
+
+    std::optional<char> peek() const;
+    std::optional<char> peek_next() const;
+    std::optional<char> advance();
+};
