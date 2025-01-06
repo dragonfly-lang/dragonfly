6be46cc74aeb0d31e0fdc42f6776bcc44d11ef82
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2025-01-06 10:06:57 +0000

    Added internal methods, attributes, and public constructors

--- include/dragon/parser.h
+++ include/dragon/parser.h
@@ -6,4 +6,16 @@
 
 
 class Parser {
+private:
+    std::vector<Token> tokens;
+    std::vector<Node> nodes;
+    size_t index;
+
+    std::optional<Token> peek() const;
+    std::optional<Token> peek_next() const;
+    std::optional<Token> advance(TokenType type);
+
+public: 
+    explicit Parser(std::vector<Token> tokens) : tokens(std::move(tokens)), index(0) {}
+    explicit Parser() : tokens(std::vector<Token>()), index(0) {}
 };
