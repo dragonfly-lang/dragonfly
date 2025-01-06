bad13a5b9f137ad7cdde7502a4107ae0bf7d303f
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2025-01-06 10:07:48 +0000

    Implemented peek() and peek_next()

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,17 @@
 #include "dragon/parser.h"
+
+std::optional<Token> Parser::peek() const {
+    if (index < tokens.size()) {
+        return tokens[index];
+    }
+
+    return std::nullopt;
+}
+
+std::optional<Token> Parser::peek_next() const {
+    if (index + 1 < tokens.size()) {
+        return tokens[index + 1];
+    }
+
+    return std::nullopt;
+}
