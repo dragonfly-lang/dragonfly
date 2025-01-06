a231a5848b01df705c381d11a51eb56d5a74ae17
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2025-01-06 10:07:20 +0000

    Added methods to parse all AST nodes

--- include/dragon/parser.h
+++ include/dragon/parser.h
@@ -3,6 +3,12 @@
 #include "token.h"
 #include "ast.h"
 
+#include "ast/variable_declaration_node.h"
+#include "ast/variable_assignment_node.h"
+#include "ast/while_loop_node.h"
+#include "ast/for_loop_node.h"
+#include "ast/if_else_node.h"
+#include "ast/function_call_node.h"
 
 
 class Parser {
@@ -18,4 +24,17 @@ private:
 public: 
     explicit Parser(std::vector<Token> tokens) : tokens(std::move(tokens)), index(0) {}
     explicit Parser() : tokens(std::vector<Token>()), index(0) {}
+
+    void reset();
+
+    std::vector<Node> parse();
+    std::vector<Node> parse(const std::vector<Token>& tokens);
+    
+    std::unique_ptr<VariableDeclarationNode> parse_variable_declaration();
+    std::unique_ptr<VariableAssignmentNode> parse_variable_assignment();
+    std::unique_ptr<WhileLoopNode> parse_while_loop();
+    std::unique_ptr<ForLoopNode> parse_for_loop();
+    std::unique_ptr<IfElseNode> parse_if_else();
+    std::unique_ptr<FunctionCallNode> parse_function_call();
+    std::unique_ptr<ExpressionNode> parse_expression();
 };
